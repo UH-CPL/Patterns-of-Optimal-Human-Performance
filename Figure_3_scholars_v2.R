8cc8937e816d7ba43f4df3388da5b1e5d31b87ee
Vitalii Zhukov <zhuko.v.italy@gmail.com>
2025-05-24 10:54:48 -0500

    Update Figure_3_scholars_v2.R

--- Figure_3_scholars_v2.R
+++ Figure_3_scholars_v2.R
@@ -15,7 +15,7 @@ source('Figure_suppl_title_cex.R')
 
 
 ### Folder where we have the data
-
+### Data may be derived from the original OSF files
 path2 <- "/Users/apple/UH-CPL/ACEs/data_02052020"
 
 #as a prototype we will use cumulative datasets
